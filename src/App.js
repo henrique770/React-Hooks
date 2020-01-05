510c7ef91827930d5214e4fe2e97009c04ff4c46
henrique770 <henrique.1360@gmail.com>
2020-01-05 16:43:58 -0300

    React hooks project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
-function App() {
+export default function App() {
+  // useState retorna um array com dois elementos, onde o primeiro é a constante que armazena aquele estado e o segundo é uma função para substituir o valor daquele estado.
+
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  // O useCallback é similar ao useMemo, mas ao invés de retornar um único resultado ele retorna uma função
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+  // useEffect recebe como parâmetro uma função callback e nela serão realizados os efeitos colaterais necessários pelo componente durante seu ciclo de vida.
+  useEffect(() => {
+    const StorageTech = localStorage.getItem('tech');
+
+    if (StorageTech) {
+      setTech(JSON.parse(StorageTech));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  // useMemo é utilizado por exemplo quando você precisa realizar cálculos de alguma variavél baseado em alguma alteração de outra variável
+  const techSize = useMemo(() => tech.length, [tech]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <strong>React Hooks</strong>
+        <ul>
+          {tech.map(t => (
+            <li key={t}>{t}</li>
+          ))}
+        </ul>
+
+        <strong>You have {techSize} technologies</strong>
+        <br />
+        <div className="formFormat">
+          <input
+            type="text"
+            value={newTech}
+            onChange={e => setNewTech(e.target.value)}
+          />
+
+          <button type="button" onClick={handleAdd}>
+            Add
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
-
-export default App;
